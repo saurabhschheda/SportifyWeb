88da1f137c32aadf9841062e37ed345024b5a6d9
saurabhschheda <saurabh.s.chheda@gmail.com>
2017-11-28 23:30:22 +0530

    Empty CSVs created

--- data/getCSV.py
+++ data/getCSV.py
@@ -6,3 +6,15 @@ def getSingleName(string):
   string[0] = string[0].replace(" ", "")
   string = string[0] + " " + string[1]
   return string
+
+def createCSVs():
+  teamCSV = open("data/team.csv", 'w')
+  teamCSV.close()
+  playerCSV = open("data/player.csv", 'w')
+  playerCSV.close()
+  venueCSV = open("data/venue.csv", 'w')
+  venueCSV.close()
+  matchCSV = open("data/match.csv", 'w')
+  matchCSV.close()
+
+createCSVs()
