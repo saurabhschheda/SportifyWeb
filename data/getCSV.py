6080fb1d64af82021aa7a3961f85e760bbe975c5
saurabhschheda <saurabh.s.chheda@gmail.com>
2017-11-29 00:18:30 +0530

    Team CSV created

--- data/getCSV.py
+++ data/getCSV.py
@@ -1,10 +1,14 @@
 import json
+import os
 
 def getSingleName(string):
   string = string.split(',')
   string.reverse()
   string[0] = string[0].replace(" ", "")
-  string = string[0] + " " + string[1]
+  try:
+    string = string[0] + " " + string[1]
+  except IndexError:
+    string = string[0]
   return string
 
 def createCSVs():
@@ -17,4 +21,34 @@ def createCSVs():
   matchCSV = open("data/match.csv", 'w')
   matchCSV.close()
 
-createCSVs()
+def extractTeamData(data):
+  id = data["team"]["id"]
+  name = data["team"]["name"]
+  league = data["statistics"]["seasons"][-1]["tournament"]["id"]
+  position = data["statistics"]["seasons"][-1]["statistics"]["group_position"]
+  try: manager = getSingleName(data["manager"]["name"])
+  except KeyError: manager = "None"
+  color1 = data["jerseys"][0]["base"]
+  try: color2 = data["jerseys"][0]["sleeve"]
+  except KeyError: color2 = color1
+  line = id + "," + name + "," + league + "," + str(position) + "," + manager
+  line = line + "," + color1 + "," + color2 + "\n"
+  teamCSV = open("data/team.csv", 'a')
+  teamCSV.write(line)
+  teamCSV.close()
+
+def extractData(filename):
+  jsonFile = open(filename, 'r')
+  data = json.loads(jsonFile.read())
+  jsonFile.close()
+  extractTeamData(data)
+  # extractVenueData(data)
+  # extractPlayerData(data)
+
+def fillCSV():
+  createCSVs()
+  path = "data/json/teams/"
+  for filename in os.listdir(path):
+    extractData(path + filename)
+
+fillCSV()
