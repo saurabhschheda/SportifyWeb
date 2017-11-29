cab1ef6054a41269bfde0a28503fda89affac696
saurabhschheda <saurabh.s.chheda@gmail.com>
2017-11-29 15:41:56 +0530

    Added routes

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var express = require('express');
-var mysql = require('mysql')
+var mysql = require('mysql');
+var path = require('path');
+var hbs = require('express-hbs');
 
 var app = express();
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs.express4({
+  defaultLayout: __dirname + '/public/views/layouts/main.hbs',
+  partialsDir: __dirname + '/public/views',
+  layoutsDir: __dirname + '/public/views/layouts'
+}));
+app.set('views', path.join(__dirname, '/public/views'));
+
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'QutabMinar5',
   database: 'Sportify'
-})
+});
 
 connection.connect(function (err) {
   if (err) throw err
@@ -15,7 +25,57 @@ connection.connect(function (err) {
 })
 
 app.get('/', function (req, res) {
-  res.send('Backend working');
+  // home.html
+  // var id = req.params.id
+  res.render('hello', { token: 'Sportify' });
+});
+
+app.get('/league/:id', function (req, res) {
+  // league.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
+});
+
+app.get('/league/:id/results', function (req, res) {
+  // matches_result.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
+});
+
+app.get('/league/:id/schedule', function (req, res) {
+  // matches_schedule.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
+});
+
+app.get('/league/:id/standings', function (req, res) {
+  // league_Standings.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
+});
+
+app.get('/team/:id', function (req, res) {
+  // team.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
+});
+
+app.get('/team/:id/results', function (req, res) {
+  // match_result.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
+});
+
+app.get('/team/:id/schedule', function (req, res) {
+  // match_schedule.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
+});
+
+app.get('/team/:id/players', function (req, res) {
+  // Team_players.html
+  var id = req.params.id;
+  res.render('hello', { token: id });
 });
 
 app.listen(3333, function () {
