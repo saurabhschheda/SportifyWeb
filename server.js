d9180aadc9d7c3fbb4814985541e1e6acbb5fe05
saurabhschheda <saurabh.s.chheda@gmail.com>
2017-11-29 12:43:16 +0530

    MySQL Connection done

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
 var express = require('express');
+var mysql = require('mysql')
+
 var app = express();
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'QutabMinar5',
+  database: 'Sportify'
+})
+
+connection.connect(function (err) {
+  if (err) throw err
+  console.log('You are now connected...')
+})
 
 app.get('/', function (req, res) {
   res.send('Backend working');
